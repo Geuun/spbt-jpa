1921f502b0dc826c08850c8cc16480ba33bab78f
KimJaeGeun <jgk9612@gmail.com>
2022-11-23 14:10:53 +0900

    feat: Add Entity Layer

---
+++ src/main/java/com/books/db/domain/author/entity/Author.java
@@ -0,0 +1,19 @@
+package com.books.db.domain.author.entity;
+
+import com.books.db.domain.book.entity.Book;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/books/db/domain/book/entity/Book.java
@@ -0,0 +1,22 @@
+package com.books.db.domain.book.entity;
+
+import com.books.db.domain.author.entity.Author;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long authorId;
+}
